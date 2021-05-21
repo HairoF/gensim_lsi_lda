fab9a465c85b92b7683c7b9dac33ba896067f5d6
Hairo <hairo.f@yandex.ru>
2021-05-21 16:59:07 +0500

    commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import requests
-import re
-from bs4 import BeautifulSoup as Bs
-print(re.match(r'\d', 'question_price_elm.text 1 2123'))
+import os.path
 
+from gensim import corpora
+from gensim.models import LsiModel
 
-# url = 'https://www.udemy.com/courses/development/?p=1'
-# headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36', 'accept-language': 'ru-RU,ru;q=0.9'}
-# html = requests.get(url, headers=headers).text
-# soup = Bs(html, 'html.parser')
-# div = soup.find_all('div', class_="course-list--container--3zXPS")
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from gensim.models.coherencemodel import CoherenceModel
+import matplotlib.pyplot as plt
+
